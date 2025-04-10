c052928dbf1e98fac3f3e06a754978eefe509776
Francizco009 <fflores393@gmail.com>
2025-04-09 23:14:22 -0500

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 
-import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "../components/ui/button";
+import { Card, CardContent } from "../components/ui/card";
 import Link from "next/link";
 
 export default function HomePage() {
